1752b1d2c6f462834f85a1858fad2784563a7940
romanlakerveld <31651999+romanlakerveld@users.noreply.github.com>
2018-01-22 13:14:55 +0100

    Added SQLassetHelper functionality

---
+++ app/src/main/java/com/example/roman/interaction/DatabaseAccess.java
@@ -0,0 +1,71 @@
+package com.example.roman.interaction;
+
+/**
+ * Created by roman on 21/01/2018.
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private  SQLiteDatabase database;
+    private static DatabaseAccess instance;
+
+    /**
+     * Private constructor to aboid object creation from outside classes.
+     *
+     * @param context
+     */
+    private DatabaseAccess(Context context) {
+        this.openHelper = new DatabaseOpenHelper(context);
+    }
+
+    /**
+     * Return a singleton instance of DatabaseAccess.
+     *
+     * @param context the Context
+     * @return the instance of DabaseAccess
+     */
+    public static DatabaseAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    /**
+     * Open the database connection.
+     */
+    public void open() {
+        this.database = openHelper.getWritableDatabase();
+    }
+
+    /**
+     * Close the database connection.
+     */
+    public void close() {
+        if (database != null) {
+            this.database.close();
+        }
+    }
+
+    /**
+     * Read all quotes from the database.
+     *
+     * @return a List of quotes
+     */
+    public String getCommon(String latin) {
+        Cursor cursor = database.rawQuery("SELECT field2 FROM englishpairs WHERE field1 = '"+latin+"'", null);
+        cursor.moveToFirst();
+        String latinName = cursor.getString(0);
+        cursor.close();
+        return latinName;
+    }
+
+}
--- app/src/main/java/com/example/roman/interaction/SpeciesSearchActivity.java
+++ app/src/main/java/com/example/roman/interaction/SpeciesSearchActivity.java
@@ -4,12 +4,39 @@ import android.database.sqlite.SQLiteDatabase;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SpeciesSearchActivity extends AppCompatActivity {
+
+    DatabaseAccess databaseAccess;
+    EditText editText;
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_species_search);
+        editText = findViewById(R.id.latin);
+        textview = findViewById(R.id.common);
+        Button button = findViewById(R.id.search);
+        button.setOnClickListener(new OnClickListener());
+    }
+
+    class OnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Log.d("CLICKED", "onClick: ");
+            databaseAccess = DatabaseAccess.getInstance(SpeciesSearchActivity.this);
+            databaseAccess.open();
+            textview.setText(databaseAccess.getCommon(editText.getText().toString()));
+            databaseAccess.close();
+            Log.d("CLICKED", "onClick: ");
+
+        }
     }
 }
