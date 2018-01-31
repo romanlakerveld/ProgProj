d7065dd107c0bea4f28fd4d2abdd3aa09d828349
romanlakerveld <31651999+romanlakerveld@users.noreply.github.com>
2018-01-31 22:34:18 +0100

    Added comments

--- app/src/main/java/com/example/roman/interaction/Interaction.java
+++ app/src/main/java/com/example/roman/interaction/Interaction.java
@@ -3,53 +3,80 @@ package com.example.roman.interaction;
 import android.content.Context;
 
 /**
- * Created by roman on 31/01/2018.
+ * Interaction class containing all information for an interaction.
  */
-
 public class Interaction {
-    // variable to hold context
     private Context context;
     private String source;
     private String target;
     private String interaction;
 
-    //save the context recieved via constructor in a local variable
-
+    /**
+     * Save the context received via constructor in a local variable
+     */
     public Interaction(Context context){
         this.context=context;
     }
 
-
-    String getSource() {
+    /**
+     *
+     * @return source of interaction
+     */
+    private String getSource() {
         return source;
     }
 
+    /**
+     * Set source of interaction, additionally this method gets the common name as well, placing it
+     * after the latin.
+     * @param source source of interaction to set
+     */
     void setSource(String source) {
+        // get common name from the database
         DatabaseAccess databaseAccess = DatabaseAccess.getInstance(context);
         databaseAccess.open();
         String common = databaseAccess.getCommon(source);
         databaseAccess.close();
 
+        // set source and concatenate with common name
         this.source = "<i>" + source + "</i>" + common;
     }
 
-    String getTarget() {
+    /**
+     *
+     * @return target of interaction
+     */
+    private String getTarget() {
         return target;
     }
 
+    /**
+     *
+     * @param target target of interaction to set
+     */
     void setTarget(String target) {
+        // get common name from database
         DatabaseAccess databaseAccess = DatabaseAccess.getInstance(context);
         databaseAccess.open();
         String common = databaseAccess.getCommon(target);
         databaseAccess.close();
 
+        // set target and concatenate with common name
         this.target = "<i>" + target + "</i>" + common;
     }
 
+    /**
+     *
+     * @return type of interaction
+     */
     public String getInteraction() {
         return interaction;
     }
 
+    /**
+     *
+     * @param interaction interaction type to set
+     */
     public void setInteraction(String interaction) {
         this.interaction = interaction;
     }
